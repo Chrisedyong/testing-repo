eef2073058604d8366a44e58911403e6be2ca1b0
Chrisedyong <ebmemaga@gmail.com>
2026-07-17 06:47:58 +0700

    commit a

---
+++ A.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    cout << "Hello world\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int tc = 1;
+    //cin >> tc;
+
+    while (tc--) {
+        solve();
+    }
+
+    return 0;
+}
